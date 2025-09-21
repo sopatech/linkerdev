85c8826e12530d3fdee2c4ec609f3ad259623445
nicklanng <rinse@rinseworld.com>
2025-09-21 17:43:33 +0100

    Release v0.1.0-alpha9

--- cmd/linkerdev/main.go
+++ cmd/linkerdev/main.go
@@ -53,7 +53,7 @@ import (
 /* ---------- Config / constants ---------- */
 
 // Version is set at build time for releases
-var version = "v0.1.0-alpha8"
+var version = "v0.1.0-alpha9"
 
 const (
 	// Labels
